4bdbd9577b1eade45f0ac39bfa67cace3ef5c243
Bophelo-G-THWALA <163294604+Bophelo-G-THWALA@users.noreply.github.com>
2024-08-14 15:26:18 +0200

    First Commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  // declare variables outside 'return' statement
+  // e.g const [Name, setName]
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
